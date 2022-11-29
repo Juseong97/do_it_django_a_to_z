6ce3d19fb15bfdfa3155d75ec8c30039d280da62
Juseong97 <118319962+Juseong97@users.noreply.github.com>
2022-11-29 17:16:59 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Post
 
-def index(requset):
+def index(request):
     posts=Post.objects.all().order_by('-pk')
     return render(
-        requset,
+        request,
         'blog/index.html',
         {
             'posts':posts,
         }
+    )
+
+def single_post_page(request, pk):
+    post=Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post':post,
+        }
+
     )
