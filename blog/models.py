042aff968cef5d9d19ae79e8d3ec99b84530e9a0
Juseong97 <118319962+Juseong97@users.noreply.github.com>
2022-11-30 16:27:18 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content=models.TextField()
+    hook_text=models.CharField(max_length=100,blank=True)
 
     head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload=models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -15,3 +17,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+
+    def get_file_name(self):
+        return os.path.basename(self.file.upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
