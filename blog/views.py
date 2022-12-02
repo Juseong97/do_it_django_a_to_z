cbe07af43ecfab6d84cfad349368186999bea5c0
Juseong97 <118319962+Juseong97@users.noreply.github.com>
2022-12-02 09:23:36 +0900

    12.02 최신화

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 # from django.shortcuts import render //FBV
-from django.views.generic import ListView,DetailView
-from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
 
 
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
     # template_name = 'blog/post_list.html' 방법 1 새로안만들고 템플릿 명 명시하기.
 
